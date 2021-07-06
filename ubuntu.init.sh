db8412abbb9b3cd48ac73d8db1a855fdcaacf1ba
chrisedrego <45294159+chrisedrego@users.noreply.github.com>
2021-07-06 21:28:22 +0530

    Update ubuntu.init.sh

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -72,4 +72,8 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E528
 sudo apt-get update
 sudo apt-get install pritunl-client-electron
 
-
+# Install Gcloud
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install apt-transport-https ca-certificates gnupg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk -y
